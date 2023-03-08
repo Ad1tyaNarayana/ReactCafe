6944733df27188f0088e242b7bb930390aac36c6
Aditya Narayana <AdityaNarayana@protonmail.com>
2023-03-08 14:05:44 +0530

    render update

--- src/Components/Auth/register.jsx
+++ src/Components/Auth/register.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cafereactapi.herokuapp.com/",
+  baseURL: "https://cafereactserver.onrender.com/",
 });
 
 export default class Cafe extends Component {
